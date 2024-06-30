e80b65d6844154750fc0593a151792aad621c6f2
‘Elena Voinu <lenka36@gmail.com>
2024-06-30 12:15:28 -0500

    Initial commit

---
+++ src/main/java/com/techjam/musicsphere/MusicSphereApplication.java
@@ -0,0 +1,13 @@
+package com.techjam.musicsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techjam/musicsphere/MusicsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techjam.musicsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
